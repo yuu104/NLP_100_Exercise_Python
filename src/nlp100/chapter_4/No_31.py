d078591d47aa86726e5f22c1e323b4113d23f7cb
yuu <yusei8171@gmail.com>
2023-03-29 11:08:39 +0900

    No.31

---
+++ src/nlp100/chapter_4/No_31.py
@@ -0,0 +1,18 @@
+from No_30 import main_30
+from typing import List
+
+def main_31() -> List[str]:
+  morphemic_sentence = main_30()
+
+  verb_surface = []
+  for sentence in morphemic_sentence:
+    for morpheme in sentence:
+      if morpheme['pos'] == '動詞':
+        verb_surface.append(morpheme['surface'])
+  
+  return verb_surface
+
+if __name__ == '__main__':
+  verb_surface = main_31()
+  print(len(verb_surface))
+  print(verb_surface[:30])
