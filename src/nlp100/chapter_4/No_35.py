d86f65591d78c6fe518623a1787e6cb26f16a255
yuu <yusei8171@gmail.com>
2023-04-07 19:54:05 +0900

    No.35

---
+++ src/nlp100/chapter_4/No_35.py
@@ -0,0 +1,18 @@
+from No_30 import main_30
+import collections
+from typing import List, Tuple
+
+def main_35() -> List[Tuple[str, int]]:
+  morphemic_sentences = main_30()
+  
+  words = []
+  for sentence in morphemic_sentences:
+    for morpheme in sentence:
+      if morpheme['pos'] != '記号':
+        words.append(morpheme['base'])
+
+  c = collections.Counter(words)
+  return c.most_common(10)
+
+if __name__ == '__main__':
+  print(main_35())
