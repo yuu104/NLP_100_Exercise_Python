8509b0157d923f1794059316cfa2ad2d58c506fb
yuu <yusei8171@gmail.com>
2023-04-09 15:58:44 +0900

    No.42

---
+++ src/nlp100/chapter_5/No_42.py
@@ -0,0 +1,14 @@
+from No_41 import main_41
+
+def main_42():
+  sentences = main_41()
+  for chunk in sentences[1]:
+    if chunk.dst == -1:
+      continue
+    morphs_text = ''.join([morph.surface for morph in chunk.morphs if morph.pos != '記号' ])
+    dst_chunk = sentences[1][chunk.dst]
+    dst_text = ''.join([morph.surface for morph in dst_chunk.morphs if morph.pos != '記号' ])
+    print(f'{morphs_text}\t{dst_text}')
+
+if __name__ == '__main__':
+  main_42()
