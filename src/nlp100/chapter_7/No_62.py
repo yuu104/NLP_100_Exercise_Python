e1df5a5d20a273f1186bad289421c9109765efca
yuu <yusei8171@gmail.com>
2023-04-23 16:03:54 +0900

    No.62

---
+++ src/nlp100/chapter_7/No_62.py
@@ -0,0 +1,14 @@
+import os
+from gensim.models import KeyedVectors
+from pprint import pprint
+
+def main_62():
+  current_path = os.path.dirname(os.path.abspath(__file__))
+  
+  model = KeyedVectors.load_word2vec_format(f'{current_path}/../../GoogleNews/GoogleNews-vectors-negative300.bin.gz', binary=True)
+
+  similar_words = model.most_similar('United_States', topn=10)
+  pprint(similar_words)
+
+if __name__ == '__main__':
+  main_62()
